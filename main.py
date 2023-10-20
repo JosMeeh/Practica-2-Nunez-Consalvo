26b61b99a910fc1cbb588c74a19cef6a7a87895b
José Nuñez <86788881+JosMeeh@users.noreply.github.com>
2023-10-20 16:15:22 -0400

    Proyecto base

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"message": "Practica 2 Jose Nunez Eduardo Consalvo"}
+
+
+@app.get("/integrantes/{id}")
+def show_integrantes(id):
+        return {"data": id}
