00db8141ee5a0b57ab01ebf907bc9d40353106d9
José Nuñez <josen1401@hotmail.com>
2023-10-20 18:01:38 -0400

    Endpoints basicos

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+
+
+class item (BaseModel):
+    id: int
+    name: str
+    emails: List[str]
 
 app = FastAPI()
-
 
 @app.get("/")
 def index():
     return {"message": "Practica 2 Jose Nunez Eduardo Consalvo"}
 
 
-@app.get("/integrantes/{id}")
+@app.get("/directory/{id}")
 def show_integrantes(id):
         return {"data": id}
+
+
+
+@app.post("/directory")
+def add_directory(item: item):
+ return {"Id": item.id, "Name": item.name, "Email": item.emails}
+
+
 
